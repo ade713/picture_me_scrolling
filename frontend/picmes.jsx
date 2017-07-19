399f27a3887bb5fe21fe138644d4dfdb12179fc5
Ade Farquhar <adefarquhar@gmail.com>
2017-07-18 17:10:22 -0700

    add session actions reducer and test

--- frontend/picmes.jsx
+++ frontend/picmes.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { signup, login, logout } from './util/session_api_util';
+import configureStore from './store/store';
+import { signup,
+         login,
+         logout,
+         receiveCurrentUser,
+         receieveErrors } from './actions/session_actions';
+// import { signup, login, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  const store = configureStore();
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Picture Me Scrolling, PicMeS</h1>, root);
 
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
+  window.receiveCurrentUser = receiveCurrentUser;
+  window.receieveErrors = receieveErrors;
   window.signup = signup;
   window.login = login;
   window.logout = logout;
