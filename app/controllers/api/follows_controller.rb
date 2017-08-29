e31917418445ff8033a1401896d7282533e54a3a
Ade Farquhar <adefarquhar@gmail.com>
2017-08-28 22:58:09 -0700

    add posts var in follows controller

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -3,15 +3,21 @@ class Api::FollowsController < ApplicationController
     @follow = Follow.new
     @follow.follower_id = current_user.id
     @follow.followee_id = params[:user_id]
+    @author = User.find_by(id: @follow.followee_id)
+    @posts = @author.posts
 
     @follow.save!
-    render 'api/posts/show'
+    render 'api/posts/index'
   end
 
   def destroy
-    @follow = current_user.followees.find_by(followee_id: params[:user_id])
+    # @follow = current_user.followees.find_by(followee_id: params[:user_id])
+    @follow = current_user.followees.find_by(followee_id: params[:followee_id])
+    @author = User.find_by(id: @follow.followee_id)
+    @posts = @author.posts
+
     @follow.destroy
-    render 'api/posts/show'
+    render 'api/posts/index'
   end
 
   private
