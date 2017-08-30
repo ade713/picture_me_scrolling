53fb1738c9afdf7110bf662d7173dd4c68e24fc7
Ade Farquhar <adefarquhar@gmail.com>
2017-08-29 18:52:30 -0700

    remove edit button tag

--- frontend/components/feed/feed_item.jsx
+++ frontend/components/feed/feed_item.jsx
@@ -83,12 +83,6 @@ class FeedItem extends React.Component {
   renderDeleteButtons() {
     if (this.post.author_id === this.props.currentUser.id) {
       return (
-        <button className="edit-post-btn">
-          <i
-            className="fa fa-pencil-square-o fa-2x"
-            id="edit-btn-icon"
-            aria-hidden="true"></i>
-        </button> ,
         <button className="delete-post-btn"
           onClick={() => this.props.deletePost(this.post)} >
           <i className="fa fa-trash fa-2x" aria-hidden="true"></i>
