7fbb772404e715b7960ba8208181f4d3bb0df8ee
Ade Farquhar <adefarquhar@gmail.com>
2017-09-01 02:57:44 -0700

    change posts

--- app/controllers/api/follows_controller.rb
+++ app/controllers/api/follows_controller.rb
@@ -3,15 +3,8 @@ class Api::FollowsController < ApplicationController
     @follow = Follow.new
     @follow.follower_id = current_user.id
     @follow.followee_id = params[:user_id]
-    @author = User.find_by(id: @follow.followee_id)
-    @posts = @posts = Post.find_by_sql([
-            'SELECT posts.*
-            FROM posts
-            LEFT JOIN users ON posts.author_id = users.id
-            LEFT JOIN follows ON users.id = follows.followee_id
-            WHERE posts.author_id = follows.followee_id AND follows.follower_id = ?',
-            current_user
-          ])
+    # @author = User.find_by(id: @follow.followee_id)
+    @posts = current_user.posts + current_user.followed_posts
 
     @follow.save!
     render 'api/posts/index'
@@ -21,14 +14,7 @@ class Api::FollowsController < ApplicationController
     @follow = current_user.followees.find_by(followee_id: params[:user_id])
     # @author = User.find_by(id: @follow.followee_id)
     # @posts = @author.posts
-    @posts = Post.find_by_sql([
-            'SELECT posts.*
-            FROM posts
-            LEFT JOIN users ON posts.author_id = users.id
-            LEFT JOIN follows ON users.id = follows.followee_id
-            WHERE posts.author_id = follows.followee_id AND follows.follower_id = ?',
-            current_user
-          ])
+    @posts = current_user.posts + current_user.followed_posts
 
     @follow.destroy
     render 'api/posts/index'
