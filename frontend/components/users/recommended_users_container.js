dd0965d0708a472e061f654fb0aee8ff60483dfa
Ade Farquhar <adefarquhar@gmail.com>
2017-09-13 15:15:37 -0700

    update recommended users

--- frontend/components/users/recommended_users_container.js
+++ frontend/components/users/recommended_users_container.js
@@ -3,5 +3,19 @@ import { connect } from 'react-redux';
 import RecommendedUsers from './recommended_users';
 import { requestUsers,
          followUser } from '../../actions/users_actions';
+import { selectUsers } from '../../reducers/selectors';
 
 
+const mapStateToProps = state => ({
+  users: selectUsers(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  requestUsers: () => dispatch(requestUsers())
+});
+
+const RecommendedUsersContainer = connect(
+  mapStateToProps, mapDispatchToProps
+)(RecommendedUsers);
+
+export default RecommendedUsers;
