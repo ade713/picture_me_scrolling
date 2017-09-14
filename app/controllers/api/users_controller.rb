18ecad2983b4ae4aa5149ef6104796cfdfd5730a
Ade Farquhar <adefarquhar@gmail.com>
2017-09-14 16:22:05 -0700

    limit rec users to 6

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -11,8 +11,8 @@ class Api::UsersController < ApplicationController
   end
 
   def index
-    # @users = User.joins(:posts).uniq
-    @users = User.all - current_user.followee_users - [current_user]
+    rec_users = User.all - current_user.followee_users - [current_user]
+    @users = rec_users.take(6)
   end
 
   private
