839713daec35f15ebe3c3db007e51e2ad72af7ef
Ade Farquhar <adefarquhar@gmail.com>
2017-09-14 01:10:36 -0700

    flex rec user items

--- frontend/components/users/rec_user_item.jsx
+++ frontend/components/users/rec_user_item.jsx
@@ -8,17 +8,19 @@ class RecUserItem extends React.Component {
   render() {
     return (
       <li className="rec-user-item">
+        <div className="rec-user-block">
+          <img 
+            className="rec-user-avatar" 
+            src={ this.props.user.avatar_url } />
+          <h3 className="rec-username">
+            { this.props.user.username }
+          </h3>
+        </div>
         <button 
           className="follow-user"
           onClick={ () => this.props.followUser(this.props.user.id) }>
           <i className="fa fa-plus-square" aria-hidden="true"></i>
         </button>
-        <img 
-          className="rec-user-avatar" 
-          src={ this.props.user.avatar_url } />
-        <h3 className="rec-username">
-          { this.props.user.username }
-        </h3>
       </li>
     );
   }
