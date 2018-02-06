992aae96fe869476e63977038a296f17410bb5b1
Ade Farquhar <adefarquhar@gmail.com>
2018-02-05 20:50:06 -0800

    finish create func

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,29 @@
 class Api::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.author_id = current_user.id
+    @comment.post_id = params[:post_id]
+    @post = @comment.post
+
+    if @comment.save
+      render 'api/posts/show'
+    else
+      render json: ['Unable to comment on post'], status: 422
+    end
+  end
+
+  def index
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:post).permit(:body)
+  end
 end
