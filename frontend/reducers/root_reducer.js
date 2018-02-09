2d4f340ecda1a3d24b573e364bc993411ee86d4b
Ade Farquhar <adefarquhar@gmail.com>
2018-02-08 17:50:03 -0800

    add comments reducer to root reducer

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -4,11 +4,13 @@ import PostsReducer from './posts_reducer';
 import SessionReducer from './session_reducer';
 import ErrorsReducer from './errors_reducer';
 import UsersReducer from './users_reducer';
+import CommentsReducer from './comments_reducer';
 
 const RootReducer = combineReducers({
   posts: PostsReducer,
   session: SessionReducer,
   users: UsersReducer,
+  comments: CommentsReducer,
   errors: ErrorsReducer
 });
 
