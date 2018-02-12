89bf44316124303741ce47185ec1216eb57931e1
Ade Farquhar <adefarquhar@gmail.com>
2018-02-11 20:04:14 -0800

    move comment props to comments container

--- frontend/components/comments/comments_container.js
+++ frontend/components/comments/comments_container.js
@@ -1,9 +1,14 @@
 import { connect } from 'react-redux';
 
 import Comments from './comments';
+import { createComment,
+         editComment, 
+         deleteComment } from '../../actions/comments_actions';
 
 const mapDispatchToProps = dispatch => ({
-  
+  createComment: comment => dispatch(createComment(comment)),
+  editComment: comment => dispatch(editComment(comment)),
+  deleteComment: comment => dispatch(deleteComment(comment))
 });
 
 const CommentsContainer = connect(
