cca0366abaeeaadd6c73755aaa5df195f218af9d
Ade Farquhar <adefarquhar@gmail.com>
2026-08-28 12:14:54 -0700

    Share scroll restoration across routed pages

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -10,67 +10,70 @@ import ResetPasswordPage from "./password_reset/reset_password_page";
 import SettingsPage from "./settings/settings_page";
 import { routes } from "../config/routes";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import { ScrollRestorationProvider } from "../util/scroll_restoration";
 
 const App = () => (
   <HashRouter>
-    <Routes>
-      <Route
-        path={routes.dashboard}
-        element={
-          <ProtectedRoute>
-            <Dashboard />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path={routes.profile}
-        element={
-          <ProtectedRoute>
-            <ProfilePage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path={routes.settings}
-        element={
-          <ProtectedRoute>
-            <SettingsPage />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path={routes.verifyEmail}
-        element={<EmailVerificationPage />}
-      />
-      <Route
-        path={routes.forgotPassword}
-        element={
-          <AuthRoute>
-            <ForgotPasswordPage />
-          </AuthRoute>
-        }
-      />
-      <Route
-        path={routes.resetPassword}
-        element={<ResetPasswordPage />}
-      />
-      <Route
-        path={routes.signup}
-        element={
-          <AuthRoute>
-            <AuthForm />
-          </AuthRoute>
-        }
-      />
-      <Route
-        path={routes.home}
-        element={
-          <AuthRoute>
-            <AuthForm />
-          </AuthRoute>
-        }
-      />
-    </Routes>
+    <ScrollRestorationProvider>
+      <Routes>
+        <Route
+          path={routes.dashboard}
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={routes.profile}
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={routes.settings}
+          element={
+            <ProtectedRoute>
+              <SettingsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path={routes.verifyEmail}
+          element={<EmailVerificationPage />}
+        />
+        <Route
+          path={routes.forgotPassword}
+          element={
+            <AuthRoute>
+              <ForgotPasswordPage />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path={routes.resetPassword}
+          element={<ResetPasswordPage />}
+        />
+        <Route
+          path={routes.signup}
+          element={
+            <AuthRoute>
+              <AuthForm />
+            </AuthRoute>
+          }
+        />
+        <Route
+          path={routes.home}
+          element={
+            <AuthRoute>
+              <AuthForm />
+            </AuthRoute>
+          }
+        />
+      </Routes>
+    </ScrollRestorationProvider>
   </HashRouter>
 );
 
